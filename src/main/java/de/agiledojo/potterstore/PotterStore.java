0a801416ed73caf45b10056de1d05962fd49707e
Christian Fischer <cf@fisch3r.de>
2018-11-15 15:36:41 +0100

    initialize application

---
+++ src/main/java/de/agiledojo/potterstore/PotterStore.java
@@ -0,0 +1,7 @@
+package de.agiledojo.potterstore;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotterStore {
+}
--- src/test/java/de/agiledojo/potterstore/PotterStoreSystemTest.java
+++ src/test/java/de/agiledojo/potterstore/PotterStoreSystemTest.java
@@ -1,5 +1,24 @@
 package de.agiledojo.potterstore;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,classes = PotterStore.class)
+@AutoConfigureMockMvc
 public class PotterStoreSystemTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void mvcExists() {
+        Assertions.assertThat(mockMvc).isNotNull();
+    }
 }
