74d1d34d56b027c05fb5df49488f2c2e22297435
Christian Fischer <cf@fisch3r.de>
2018-11-16 17:38:02 +0100

    setup request test

--- src/test/java/de/agiledojo/potterstore/PotterStoreSystemTest.java
+++ src/test/java/de/agiledojo/potterstore/PotterStoreSystemTest.java
@@ -3,22 +3,35 @@ package de.agiledojo.potterstore;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,classes = PotterStore.class)
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = RANDOM_PORT,classes = PotterStore.class)
 public class PotterStoreSystemTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    @LocalServerPort
+    int port;
 
     @Test
-    public void mvcExists() {
-        Assertions.assertThat(mockMvc).isNotNull();
+    public void applicationListensToAPort() {
+        Assertions.assertThat(port).isNotEqualTo(0);
+    }
+
+    @Test
+    public void singlePriceIsReturnedForOneBook() {
+        given()
+                .port(port)
+        .when()
+                .post("/price")
+        .then().assertThat()
+                .body("amount", response -> equalTo("8"))
+                .body("currency", value -> equalTo("€"));
     }
 }
