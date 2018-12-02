ef58fdda14ec1feccf1567556aa7beb87a529797
Christian Fischer <cf@fisch3r.de>
2018-12-02 12:58:35 +0100

    refactoring

--- src/main/java/de/agiledojo/potterstore/app/ParameterController.java
+++ src/main/java/de/agiledojo/potterstore/app/ParameterController.java
@@ -2,6 +2,7 @@ package de.agiledojo.potterstore.app;
 
 import de.agiledojo.potterstore.Price;
 import de.agiledojo.potterstore.ParameterRepository;
+import de.agiledojo.potterstore.PriceCalculation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,20 +19,9 @@ public class ParameterController {
         this.repository = repository;
     }
 
-    @RequestMapping(path="/parameters/{id}",method = RequestMethod.PUT)
-    public void parameters(@PathVariable("id") String parameterId, @RequestBody JSONPrice price) {
-        var singleBookPrice = new Price() {
-
-            @Override
-            public BigDecimal getAmount() {
-                return price.getAmount();
-            }
-
-            @Override
-            public Currency getCurrency() {
-                return Currency.getInstance(price.getCurrency());
-            }
-        };
+    @RequestMapping(path="/parameters/price",method = RequestMethod.PUT)
+    public void parameters(@RequestBody JSONPrice price) {
+        var singleBookPrice = PriceCalculation.price(price.getAmount(),Currency.getInstance(price.getCurrency()));
         repository.saveOrUpdateSingleBookPrice(singleBookPrice);
     }
 }
